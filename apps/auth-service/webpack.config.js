336fcf64758e096643ffc29419bb32c57682b2e1
Kuameyangseo <iamyangseo@gmail.com>
2025-09-26 01:02:55 +0000

    first

---
+++ apps/auth-service/webpack.config.js
@@ -0,0 +1,30 @@
+const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
+const { join } = require('path');
+
+module.exports = {
+  output: {
+    path: join(__dirname, 'dist'),
+    ...(process.env.NODE_ENV !== 'production' && {
+      devtoolModuleFilenameTemplate: '[absolute-resource-path]',
+    }),
+    resolve: {
+      alias:{
+        '@package': join(__dirname, 'src/app/package/'),
+      },
+      extensions: ['.ts', '.js'],
+    }
+  },
+  plugins: [
+    new NxAppWebpackPlugin({
+      target: 'node',
+      compiler: 'tsc',
+      main: './src/main.ts',
+      tsConfig: './tsconfig.app.json',
+      assets: ["./src/assets"],
+      optimization: false,
+      outputHashing: 'none',
+      generatePackageJson: true,
+      sourceMaps: true,
+    })
+  ],
+};
